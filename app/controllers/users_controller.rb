125dd51a69c5d12d9a57d224aef8b622ecb1310e
Jonas <jonas.preisler@gmail.com>
2018-05-15 11:18:15 +0800

    user params made properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   end
 
   private
-  def users_params
-    params.require(:post).permit(:avatar)
+  def user_params
+    params.require(:user).permit(:avatar)
   end
 
 end
